037512b1583a47653bec7545f711a30c549366ac
SunnyHsu <z99876664@gmail.com>
2025-10-05 02:26:19 +0800

    Restore the build

--- Calc.py
+++ Calc.py
@@ -2,7 +2,7 @@ class Calculator:
     def add(self, a, b):
         return a + b
     def sub(self, a, b):
-        return a + b
+        return a - b
     def mul(self, a, b):
         return a * b
     def div(self, a, b):
